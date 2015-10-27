113506dc1ce1d80590fbced2dea3e7e697b2321c
LaNMaSteR53 <tjt1980@gmail.com>
2015-10-26 21:43:24 -0400

    Removed the session cookie HttpOnly flag.
    Set the static folder to serve from the web root.

--- pwnedhub/__init__.py
+++ pwnedhub/__init__.py
@@ -12,8 +12,14 @@ SECRET_KEY = 'development key'
 PW_ENC_KEY = 'sekrit'
 UPLOAD_FOLDER = '/tmp/artifacts'
 ALLOWED_EXTENSIONS = set(['txt', 'xml'])
+# ;;session cookie with HttpOnly disabled
+SESSION_COOKIE_HTTPONLY = False
+#SESSION_REFRESH_EACH_REQUEST = False # not available in 0.10.1
 
-app = Flask(__name__)
+# setting the static_url_path to blank serves static
+# files from the web root, allowing for robots.txt
+# ;;verbose robots.txt file
+app = Flask(__name__, static_url_path='')
 app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
