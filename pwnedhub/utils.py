d2e044ab44ca20be446141d17152ebfacd7831d1
LaNMaSteR53 <tjt1980@gmail.com>
2018-06-13 11:47:36 -0400

    Added a JavaScript escape function to the utils module to support JavaScript context XSS remediation.

--- pwnedhub/utils.py
+++ pwnedhub/utils.py
@@ -18,3 +18,27 @@ import os
 
 def get_token(n=40):
     return binascii.hexlify(os.urandom(n))
+
+# borrowed from Django and modified to work in Python 2
+
+_js_escapes = {
+    ord(u'\\'): u'\\u005C',
+    ord(u'\''): u'\\u0027',
+    ord(u'"'): u'\\u0022',
+    ord(u'>'): u'\\u003E',
+    ord(u'<'): u'\\u003C',
+    ord(u'&'): u'\\u0026',
+    ord(u'='): u'\\u003D',
+    ord(u'-'): u'\\u002D',
+    ord(u';'): u'\\u003B',
+    ord(u'`'): u'\\u0060',
+    ord(u'\u2028'): u'\\u2028',
+    ord(u'\u2029'): u'\\u2029'
+}
+
+# escape every ASCII character with a value less than 32.
+_js_escapes.update((ord(u'%c' % z), u'\\u%04X' % z) for z in range(32))
+
+def escapejs(value):
+    """hex encode characters for use in JavaScript strings."""
+    return unicode(value).translate(_js_escapes)
