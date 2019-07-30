5ef1f326051f6c354fc451284087e71ca35f7195
LaNMaSteR53 <tjt1980@gmail.com>
2019-07-30 00:40:43 -0400

    Added arbitrary reflected origin to the PwnedAPI.

--- pwnedapi/config.py
+++ pwnedapi/config.py
@@ -12,7 +12,8 @@ class BaseConfig(object):
     ALLOWED_MIMETYPES = set(['text/plain', 'application/xml', 'image/jpeg', 'image/png', 'image/gif', 'application/pdf'])
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://pwnedhub:dbconnectpass@localhost/pwnedhub'
-    CORS_ORIGINS = ['http://pwnedhub.com:5000', 'http://www.pwnedhub.com:5000']
+    #[vuln] arbitrary reflected origin, providing a list enables whitelisting
+    #CORS_ORIGINS = ['http://pwnedhub.com:5000', 'http://www.pwnedhub.com:5000']
 
 class Development(BaseConfig):
 
@@ -26,4 +27,5 @@ class Test(BaseConfig):
 class Production(BaseConfig):
 
     SECRET_KEY = 'M>\n\xb2\xa9B\xae\x8cL~\x0b\xc4\x19\r/GR6\xca\xd1^o\xa3$'
-    CORS_ORIGINS = ['http://pwnedhub.com', 'http://www.pwnedhub.com']
+    #[vuln] arbitrary reflected origin, providing a list enables whitelisting
+    #CORS_ORIGINS = ['http://pwnedhub.com', 'http://www.pwnedhub.com']
