3a84b12171ac15ae9a9d1a2d2c174ea6faee48d2
lanmaster53 <tjt1980@gmail.com>
2020-07-09 14:46:17 -0400

    Reorganized all VueJS component-related style sheet files.

--- common/static/vue/components/modal.js
+++ common/static/vue/components/modal.js
@@ -1,26 +1,3 @@
-Vue.component("password-field", {
-    props: {
-        name: String,
-        value: String,
-    },
-    template: `
-        <div class="flex-column" style="position: relative;">
-            <input v-bind:name="name" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" v-bind:type="passwordFieldType" />
-            <input type="button" class="show" tabindex="-1" v-on:click="toggleShow" value="show" />
-        </div>
-    `,
-    data: function() {
-        return  {
-            passwordFieldType: "password",
-        }
-    },
-    methods: {
-        toggleShow: function() {
-            this.passwordFieldType = this.passwordFieldType === "password" ? "text" : "password"
-        },
-    },
-});
-
 Vue.component("modal", {
     template: `
         <transition name="modal">
