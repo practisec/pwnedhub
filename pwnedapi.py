0fd70c8caf89a9725f80733e2427326a3848df46
lanmaster53 <tjt1980@gmail.com>
2020-09-09 11:41:50 -0400

    Added the PwnedSPA development launch script and updated all development launch scripts to work within Docker containers.

--- pwnedapi.py
+++ pwnedapi.py
@@ -1,5 +1,7 @@
 from pwnedapi import create_app
 
+# docker-compose run -p 5002:5002 app python ./pwnedapi.py
+
 app, socketio = create_app()
 if __name__ == '__main__':
-    socketio.run(app, port=5002)
+    socketio.run(app, host='0.0.0.0', port=5002)
--- pwnedhub.py
+++ pwnedhub.py
@@ -1,5 +1,7 @@
 from pwnedhub import create_app
 
+# docker-compose run -p 5000:5000 app python ./pwnedhub.py
+
 app = create_app()
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(host='0.0.0.0', port=5000)
