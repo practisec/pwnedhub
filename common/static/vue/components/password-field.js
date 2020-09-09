8505276e1d0e208ddf209ed3b9e25d42061e816d
lanmaster53 <tjt1980@gmail.com>
2020-09-09 11:31:59 -0400

    Modified the password-field vue component to be more independent with inline styles.

--- common/static/vue/components/password-field.js
+++ common/static/vue/components/password-field.js
@@ -6,7 +6,7 @@ Vue.component("password-field", {
     template: `
         <div class="flex-column" style="position: relative;">
             <input v-bind:name="name" v-bind:value="value" v-on:input="$emit('input', $event.target.value)" v-bind:type="passwordFieldType" />
-            <input type="button" class="show" tabindex="-1" v-on:click="toggleShow" value="show" />
+            <input type="button" tabindex="-1" v-on:click="toggleShow" value="show" style="background-color: transparent; position: absolute; right: 0; top: 0; border: 0;" />
         </div>
     `,
     data: function() {
