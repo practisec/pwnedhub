practisec/pwnedhub

commit 56ab00bbb23b02fd17777eadeee5d36ab75983e9

Refactored how common static files are incorporated into to all applications.
Completely rebuilt the out-of-band email system.

Made some minor code structure changes.

Cleaned up some unused files.

pwnedhub/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -13,7 +13,7 @@ def create_app(config='Development'):
 
     # setting the static_url_path to blank serves static
     # files from the web root, allowing for robots.txt, etc.
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('pwnedhub.config.{}'.format(config.title()))
 
     app.redis = Redis.from_url(app.config['REDIS_URL'])
@@ -46,12 +46,15 @@ def create_app(config='Development'):
         from markdown import markdown
         return Markup(markdown(data or '', extensions=app.config['MARKDOWN_EXTENSIONS']))
 
-    from pwnedhub.views.core import core
-    from pwnedhub.views.auth import auth
-    from pwnedhub.views.errors import errors
-    app.register_blueprint(core)
-    app.register_blueprint(auth)
-    app.register_blueprint(errors)
+    StaticBlueprint = Blueprint('common', __name__, static_url_path='/static/common', static_folder='../common/static')
+    app.register_blueprint(StaticBlueprint)
+
+    from pwnedhub.views.core import blp as CoreBlueprint
+    from pwnedhub.views.auth import blp as AuthBlueprint
+    from pwnedhub.views.errors import blp as ErrorsBlueprint
+    app.register_blueprint(CoreBlueprint)
+    app.register_blueprint(AuthBlueprint)
+    app.register_blueprint(ErrorsBlueprint)
 
     return app
 

pwnedapi/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_cors import CORS
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
@@ -12,7 +12,7 @@ socketio = SocketIO()
 def create_app(config='Development'):
 
     # setting the static_url_path to blank serves static files from the web root
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('pwnedapi.config.{}'.format(config.title()))
 
     app.redis = Redis.from_url(app.config['REDIS_URL'])
@@ -49,8 +49,11 @@ def create_app(config='Development'):
     cors.init_app(app)
     socketio.init_app(app, cors_allowed_origins=app.config['ALLOWED_ORIGINS'])
 
-    from pwnedapi.views.api import resources
-    app.register_blueprint(resources)
+    StaticBlueprint = Blueprint('common', __name__, static_url_path='/static/common', static_folder='../common/static')
+    app.register_blueprint(StaticBlueprint)
+
+    from pwnedapi.views.api import blp as ApiBlueprint
+    app.register_blueprint(ApiBlueprint)
 
     from pwnedapi.views import websockets
 

pwnedconfig/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 def create_app(config='Development'):
 
     # setting the static_url_path to blank serves static
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('pwnedconfig.config.{}'.format(config.title()))
 
     db.init_app(app)
@@ -19,8 +19,13 @@ def create_app(config='Development'):
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
 
-    from pwnedconfig.views.core import core
-    app.register_blueprint(core)
+    StaticBlueprint = Blueprint('common', __name__, static_url_path='/static/common', static_folder='../common/static')
+    app.register_blueprint(StaticBlueprint)
+
+    from pwnedconfig.views.config import blp as ConfigBlurprint
+    from pwnedconfig.views.email import blp as EmailBlurprint
+    app.register_blueprint(ConfigBlurprint)
+    app.register_blueprint(EmailBlurprint)
 
     return app
 

pwnedspa/__init__.py
@@ -1,16 +1,19 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 def create_app(config='Development'):
 
     # setting the static_url_path to blank serves static
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('pwnedspa.config.{}'.format(config.title()))
 
     # misc jinja configuration variables
     app.jinja_env.trim_blocks = True
     app.jinja_env.lstrip_blocks = True
 
-    from pwnedspa.views.core import core
-    app.register_blueprint(core)
+    StaticBlueprint = Blueprint('common', __name__, static_url_path='/static/common', static_folder='../common/static')
+    app.register_blueprint(StaticBlueprint)
+
+    from pwnedspa.views.core import blp as CoreBlueprint
+    app.register_blueprint(CoreBlueprint)
 
     return app

pwnedsso/__init__.py
@@ -5,12 +5,12 @@ db = SQLAlchemy()
 
 def create_app(config='Development'):
 
-    app = Flask(__name__, static_url_path='')
+    app = Flask(__name__, static_url_path='/static')
     app.config.from_object('pwnedsso.config.{}'.format(config.title()))
 
     db.init_app(app)
 
-    from pwnedsso.views.sso import sso
-    app.register_blueprint(sso)
+    from pwnedsso.views.sso import blp as SsoBlueprint
+    app.register_blueprint(SsoBlueprint)
 
     return app

commit 61d737ee504ad47366575f104122eaa6d01c15ec

Fixed a bug introduced by 56ab00b that broke the mobile PwnedHub page.

pwnedhub/__init__.py
@@ -49,7 +49,7 @@ def create_app(config='Development'):
     @app.before_request
     def render_mobile():
         if any(x in request.user_agent.string.lower() for x in ['android', 'iphone', 'ipad']):
-            if not request.endpoint.startswith('static'):
+            if not request.endpoint in ('static', 'common.static'):
                 return render_template('mobile.html')
 
     @app.before_request